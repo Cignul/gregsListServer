425eba8375695bd4fd652679252cfbdb65b70ed4
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-08-21 14:40:50 -0600

    updated something

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ let port = 3000
 //database connection
 require('./server-assets/db/mlab-config')
 
+server.use(bp.json())
+server.use(bp.urlencoded({
+  extended: true
+}))
+
+//Routes
+//let pizzaRoutes = require('./server-assets/routes/')
